aa8f7e7e412e0126a8ba3366352b47bfb678d676
Daria <bondarika@yandex.ru>
2025-01-13 23:57:59 +0300

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,63 @@
-import './App.scss'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import React from 'react'
-import routes from './routes/routes'
+import { ReactNode } from "react";
+import {
+  useNavigate,
+  Route,
+  Routes,
+  Navigate,
+  useResolvedPath,
+  BrowserRouter,
+  To,
+  useLocation,
+} from "react-router-dom";
+import "./App.scss";
+import ProfilePage from "./pages/ProfilePage";
+import StorePage from "./pages/StorePage";
+import TaskPage from "./pages/TaskPage";
 
-function App(): React.ReactElement{
-  const router = createBrowserRouter(routes)
-
+export default function App() {
   return (
-    <div className='app'>
-        <RouterProvider router={router} />
-    </div>  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to="/profile" />} />
+        <Route path="/*" element={<Page />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+function Page() {
+  return (
+    <>
+      <div role="tablist">
+        <RouterTab to="/profile">Tab 1</RouterTab>
+        <RouterTab to="/store">Tab 2</RouterTab>
+        <RouterTab to="/tasks">Tab 3</RouterTab>
+      </div>
+      <Routes>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/store" element={<StorePage />} />
+        <Route path="/tasks" element={<TaskPage />} />
+      </Routes>
+    </>
+  );
+}
+
+interface RouterTabProps {
+  children: ReactNode;
+  to: To;
+}
+
+function RouterTab({ children, to }: RouterTabProps) {
+  const navigate = useNavigate();
+
+  const { pathname: toPathname } = useResolvedPath(to);
+  const { pathname: locationPathname } = useLocation();
+
+  const selected = locationPathname.startsWith(toPathname);
+
+  return (
+    <button role="tab" onClick={() => navigate(to)} aria-selected={selected}>
+      {children}
+    </button>
+  );
+}
---
+++ src/components/RouterTab.tsx
@@ -0,0 +1,29 @@
+﻿import { ReactNode } from "react";
+import {
+  useLocation,
+  useNavigate,
+  useResolvedPath,
+  To,
+} from "react-router-dom";
+
+interface RouterTabProps {
+  children: ReactNode;
+  to: To;
+}
+
+function RouterTab({ children, to }: RouterTabProps) {
+  const navigate = useNavigate();
+
+  const { pathname: toPathname } = useResolvedPath(to);
+  const { pathname: locationPathname } = useLocation();
+
+  const selected = locationPathname.startsWith(toPathname);
+
+  return (
+    <button role="tab" onClick={() => navigate(to)} aria-selected={selected}>
+      {children}
+    </button>
+  );
+}
+
+export default RouterTab;
