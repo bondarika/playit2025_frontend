49b51abf57590b2c793e852ce772736781fcbf43
bondarika <bondarika1@gmail.com>
2025-02-14 18:58:52 +0300

    jsonchik

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -1,37 +1,40 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 
 function TaskPage(): React.ReactElement {
-  // async function fetchTasks() {
-  //   try {
-  //     const response = await fetch(
-  //       "http://localhost:8000/playit/tasks/get-all",
-  //       {
-  //         method: "GET",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         credentials: "include",
-  //       }
-  //     );
+  const API_BASE_URL = "http://188.225.58.99:8000/playit";
+  const [error, setError] = useState<string>(""); // Для хранения ошибок
+  async function fetchTasks() {
+    try {
+      const response = await fetch(
+        `API_BASE_URL/tasks/get-all`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
 
-  //     if (!response.ok) {
-  //       throw new Error(`Ошибка: ${response.statusText}`);
-  //     }
+      if (!response.ok) {
+        throw new Error(`Ошибка: ${response.statusText}`);
+      }
 
-  //     const data = await response.json();
-  //     if (data.status === "success") {
-  //       setUser(data.user); // Устанавливаем данные пользователя в состояние
-  //     } else {
-  //       setError(data.message); // Обрабатываем ошибки с сервера
-  //     }
-  //   } catch (error) {
-  //     setError("Ошибка при загрузке данных пользователя.");
-  //     console.error(
-  //       "Ошибка при отправке данных на сервер:",
-  //       error instanceof Error ? error.message : error
-  //     );
-  //   }
-  // }
+      const data = await response.json();
+      console.log(data)
+      // if (data.status === "success") {
+      //   setUser(data.user); // Устанавливаем данные пользователя в состояние
+      // } else {
+      //   setError(data.message); // Обрабатываем ошибки с сервера
+      // }
+    } catch (error) {
+      setError("Ошибка при загрузке данных.");
+      console.error(
+        "Ошибка при отправке данных на сервер:",
+        error instanceof Error ? error.message : error
+      );
+    }
+  }
   return (
     <div>
       <h1>Задания</h1>
