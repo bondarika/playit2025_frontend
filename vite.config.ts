855b1762d8bba26633fd765cc6716d5babaaa45c
bondarika <bondarika@yandex.ru>
2025-02-17 18:13:39 +0300

    я это уже ненавижу

--- vite.config.ts
+++ vite.config.ts
@@ -3,43 +3,26 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { resolve } from "path";
 
-// export default defineConfig({
-//   plugins: [react(), svgr({})],
-//   server: {
-//     allowedHosts: ["it-otdel.space"],
-//     host: true,
-//     port: 5173,
-//     hmr: {
-//       protocol: "wss",
-//       host: "it-otdel.space",
-//       port: 5173,
-//       clientPort: 5173,
-//     },
-//     watch: {
-//       usePolling: true,
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       "@": resolve(__dirname, "./src/"),
-//     },
-//   },
-// });
-
-
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr({})],
   server: {
-    host: true, // Доступен в локальной сети
+    allowedHosts: ["it-otdel.space"],
+    host: true,
     port: 5173,
     hmr: {
-      protocol: "wss", // Для HTTPS
-      host: "it-otdel.space"
-    }
+      protocol: "wss",
+      host: "it-otdel.space",
+      port: 5173,
+      clientPort: 5173,
+    },
+    watch: {
+      usePolling: true,
+    },
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src/")
-    }
-  }
+      "@": resolve(__dirname, "./src/"),
+    },
+  },
 });
+
