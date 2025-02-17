566cf25fd66977f05b53c24f03cf7c0d74fae486
Lef <116380867+Stremilov@users.noreply.github.com>
2025-02-17 19:49:44 +0300

    откат

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -4,8 +4,7 @@ import icons from "../../assets/icons";
 
 function ProfilePage(): React.ReactElement {
   // "https://it-otdel.space/playit";
-  // const API_BASE_URL = "https://it-otdel.space/playit";
-  const API_BASE_URL = "http://localhost:8000/playit";
+  const API_BASE_URL = "https://it-otdel.space/playit";
   const [error, setError] = useState<string>(""); // Для хранения ошибок
   interface User {
     id: number;
