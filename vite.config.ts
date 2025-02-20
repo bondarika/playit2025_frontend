d08cf56c55b5d921b155202c5008849b0b6d4495
bondarika <bondarika@yandex.ru>
2025-02-20 14:14:57 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,35 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { resolve } from "path";
+import fs from "fs";
 
+// export default defineConfig({
+//   plugins: [react(), svgr({})],
+//   server: {
+//     allowedHosts: ["it-otdel.space"],
+//     host: true,
+//     port: 5173,
+//     hmr: false,
+//     watch: {
+//       usePolling: true,
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       "@": resolve(__dirname, "./src/"),
+//     },
+//   },
+// });
 export default defineConfig({
   plugins: [react(), svgr({})],
   server: {
-    proxy: {
-      "/ws": {
-        target: "wss://it-otdel.space",
-        changeOrigin: true,
-        ws: true,
-        secure: true,
-      },
-    },
-
     allowedHosts: ["it-otdel.space"],
-    host: true,
-    port: 5173,
-    hmr: false,
-    watch: {
-      usePolling: true,
+    https: {
+      key: fs.readFileSync("localhost+2-key.pem"),
+      cert: fs.readFileSync("localhost+2.pem"),
     },
+    host: "localhost",
+    port: 5173,
   },
   resolve: {
     alias: {
