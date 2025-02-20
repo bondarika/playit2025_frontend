9902d97ba13a521a0da14ed9dd7d342e82b3f83b
bondarika <bondarika@yandex.ru>
2025-02-20 16:58:38 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -25,10 +25,10 @@ export default defineConfig({
   plugins: [react(), svgr({})],
   server: {
     allowedHosts: ["it-otdel.space"],
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "certs", "localhost+2-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "certs", "localhost+2.pem")),
-    },
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, "certs", "localhost+2-key.pem")),
+    //   cert: fs.readFileSync(path.resolve(__dirname, "certs", "localhost+2.pem")),
+    // },
     host: true,
     port: 5173,
   },
