59f69259c0b1ea6fd5ef4bb4e5e9ff7133e8fe5a
bondarika <bondarika@yandex.ru>
2025-03-02 00:03:54 +0300

    task

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,15 +1,18 @@
-﻿import "./styles.scss"
-import Coin from "@/assets/icons/coin/coin_icon.svg?react"
-function Task () {
-
-    return (
-      <div className="task">
-        <div className="task__price">
-          <p>30</p>
-          <Coin/>
-        </div>
+﻿import "./styles.scss";
+import Coin from "@/assets/icons/coin/coin_icon.svg?react";
+function Task() {
+  return (
+    <div className="task">
+      <div className="task__info">
+        <h2 className="task__character">Оборотень</h2>
+        <h3 className="task__description">blablabla</h3>
       </div>
-    );
+      <div className="task__points">
+        <h3 className="task__description">30</h3>
+        <Coin />
+      </div>
+    </div>
+  );
 }
 
-export default Task
+export default Task;
