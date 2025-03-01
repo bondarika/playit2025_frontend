6bb8eaab8e8b67078c0fd351d6f2e91038349cdb
bondarika <bondarika@yandex.ru>
2025-03-02 00:18:44 +0300

    fix

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -5,8 +5,10 @@ function Task() {
     <div className="task">
       <div className="task__info">
         <p className="task__character">Оборотень</p>
-        <div style={{width: "118px", height: "40px"}}>
-          <p className="task__description">blablabla</p>
+        <div style={{ width: "118px", height: "40px" }}>
+          <p className="task__description">
+            Люблю сияющие бусы из раковин и долгие прогулки…
+          </p>
         </div>
       </div>
       <div className="task__points">
