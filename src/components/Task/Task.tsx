d3e9d2a2f44bbbdcfeadce481cd1fec0b4019ea4
bondarika <bondarika@yandex.ru>
2025-03-02 00:12:30 +0300

    task styles

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -5,7 +5,9 @@ function Task() {
     <div className="task">
       <div className="task__info">
         <p className="task__character">Оборотень</p>
-        <p className="task__description">blablabla</p>
+        <div style={{width: "118px", height: "40px"}}>
+          <p className="task__description">blablabla</p>
+        </div>
       </div>
       <div className="task__points">
         <p className="task__description">30</p>
