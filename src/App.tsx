577fc6b0c09819092d9e50ab8abcbdc4ee7074f8
bondarika <bondarika@yandex.ru>
2025-03-03 00:45:05 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ import WebApp from "@twa-dev/sdk";
 
 export default function App() {
   WebApp.expand();
+  if (window.visualViewport) {
+    window.visualViewport.addEventListener("resize", () => {
+      document.body.style.height = window.visualViewport?.height + "px";
+    });
+  }
+  // This will ensure user never overscroll the page
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) window.scrollTo(0, 0);
+  });
   return (
     <BrowserRouter>
       <Routes>
