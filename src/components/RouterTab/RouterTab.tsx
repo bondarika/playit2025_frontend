c009c908ffcc8b12ca47c973c5deeea03d9e2063
bondarika <bondarika@yandex.ru>
2025-03-03 13:27:30 +0300

    fix

--- src/components/RouterTab/RouterTab.tsx
+++ src/components/RouterTab/RouterTab.tsx
@@ -1,21 +1,17 @@
-﻿import { useNavigate, useLocation } from "react-router-dom";
+﻿import { useNavigate, useLocation, useResolvedPath } from "react-router-dom";
 import { RouterTabProps } from "./types";
 import "./styles.scss";
 
 function RouterTab({ children, to }: RouterTabProps) {
   const navigate = useNavigate();
 
-  const toPathname = `/${to}`;
+  const { pathname: toPathname } = useResolvedPath(to);
   const { pathname: locationPathname } = useLocation();
 
   const selected = locationPathname.startsWith(toPathname);
 
   return (
-    <button
-      role="tab"
-      onClick={() => navigate(`/${to}`)}
-      aria-selected={selected}
-    >
+    <button role="tab" onClick={() => navigate(to)} aria-selected={selected}>
       {children}
     </button>
   );
--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,4 +1,4 @@
-﻿import { Routes, Route, Navigate } from "react-router-dom";
+﻿import { Routes, Route, useLocation } from "react-router-dom";
 import RouterTab from "../RouterTab/RouterTab";
 import ProfilePage from "../../pages/Profile/ProfilePage";
 import StorePage from "../../pages/StorePage";
@@ -7,39 +7,44 @@ import Shop from "@/assets/icons/shop/shop_icon.svg?react";
 import Tasks from "@/assets/icons/tasks/tasks_icon.svg?react";
 import "./styles.scss";
 
-const TabRoutes = () => (
-  <Routes>
-    <Route path="tasks" element={<TaskPage />} />
-    <Route path="profile" element={<ProfilePage />} />
-    <Route path="store" element={<StorePage />} />
-    <Route path="*" element={<Navigate to="profile" />} />
-  </Routes>
-);
-
 function TabBar() {
+  const location = useLocation();
+  const selected = location.pathname.split("/")[1];
   return (
     <>
       <div style={{ width: "100%" }} key={location.pathname}>
         <div className="tabbar">
-          <RouterTab to="tasks">
+          <RouterTab to="/tasks">
             <div className="tabbar__tab">
-              <Tasks />
+              <Tasks
+                className={`icon-${
+                  selected === "tasks" ? "active" : "inactive"
+                }`}
+              />
               <p>задания</p>
             </div>
           </RouterTab>
-          <RouterTab to="profile">
+          <RouterTab to="/profile">
             <div></div>
           </RouterTab>
-          <RouterTab to="store">
+          <RouterTab to="/store">
             <div className="tabbar__tab">
-              <Shop />
+              <Shop
+                className={`icon-${
+                  selected === "shop" ? "active" : "inactive"
+                }`}
+              />
               <p>магазин</p>
             </div>
           </RouterTab>
         </div>
       </div>
 
-      <TabRoutes />
+      <Routes>
+        <Route path="/tasks" element={<TaskPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/store" element={<StorePage />} />
+      </Routes>
     </>
   );
 }
