0e8e8b2cd5e717dac0820cf5783e06f9f7506c27
bondarika <bondarika@yandex.ru>
2025-03-03 12:10:28 +0300

    fix

--- src/components/RouterTab/RouterTab.tsx
+++ src/components/RouterTab/RouterTab.tsx
@@ -1,15 +1,14 @@
-﻿import { useNavigate, useResolvedPath, useLocation } from "react-router-dom";
+﻿import { useNavigate, useLocation } from "react-router-dom";
 import { RouterTabProps } from "./types";
 import "./styles.scss";
 
 function RouterTab({ children, to }: RouterTabProps) {
   const navigate = useNavigate();
 
-  const { pathname: toPathname } = useResolvedPath(`/${to}`);
+  const toPathname = `/${to}`;
   const { pathname: locationPathname } = useLocation();
 
   const selected = locationPathname.startsWith(toPathname);
-  
 
   return (
     <button role="tab" onClick={() => navigate(to)} aria-selected={selected}>
