f69a3228a70220782f3ea0e70bf1ed707636d092
bondarika <bondarika@yandex.ru>
2025-03-03 14:34:04 +0300

    fix router

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,49 +1,52 @@
-﻿import { useLocation, NavLink } from "react-router-dom";
+﻿import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import RouterTab from "../RouterTab/RouterTab";
+import ProfilePage from "../../pages/Profile/ProfilePage";
+import StorePage from "../../pages/StorePage";
+import TaskPage from "../../pages/TaskPage/TaskPage";
 import Shop from "@/assets/icons/shop/shop_icon.svg?react";
 import Tasks from "@/assets/icons/tasks/tasks_icon.svg?react";
 import "./styles.scss";
 
 function TabBar() {
   const location = useLocation();
-
+  const selected = location.pathname.split("/")[1];
   return (
-    <div style={{ width: "100%" }} key={location.pathname}>
-      <div className="tabbar">
-        <NavLink
-          to="/tasks"
-          className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""
-          }
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Tasks />
-          <p>задания</p>
-        </NavLink>
-
-        <NavLink to="/profile">
-          <div></div>
-        </NavLink>
-
-        <NavLink
-          to="/store"
-          className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""
-          }
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Shop />
-          <p>магазин</p>
-        </NavLink>
+    <>
+      <div style={{ width: "100%" }} key={location.pathname}>
+        <div className="tabbar">
+          <RouterTab to="/tasks">
+            <div className="tabbar__tab">
+              <Tasks
+                className={`icon-${
+                  selected === "tasks" ? "active" : "inactive"
+                }`}
+              />
+              <p>задания</p>
+            </div>
+          </RouterTab>
+          <RouterTab to="/profile">
+            <div></div>
+          </RouterTab>
+          <RouterTab to="/store">
+            <div className="tabbar__tab">
+              <Shop
+                className={`icon-${
+                  selected === "store" ? "active" : "inactive"
+                }`}
+              />
+              <p>магазин</p>
+            </div>
+          </RouterTab>
+        </div>
       </div>
-    </div>
+
+      <Routes>
+        <Route index element={<Navigate to="/profile" />} />
+        <Route path="/tasks" element={<TaskPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/store" element={<StorePage />} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -36,7 +36,7 @@ function StorePage(): React.ReactElement {
   return (
     <div>
       <header>
-        <h1 className="header">Магазин</h1>
+        <h1 className="header">магазин</h1>
         {/* <img src={icons["coin_bag"]} alt="Баланс" /> */}
       </header>
     </div>
