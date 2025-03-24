7b5e56630cb471a65973f5877c49cb0a1d4fd2b6
bondarika <bondarika@yandex.ru>
2025-03-24 17:26:38 +0300

    fix

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,41 +1,47 @@
-﻿import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-import RouterTab from "../RouterTab/RouterTab";
-import Store from "../../pages/Store/Store";
-import Task from "../../pages/Tasks/Tasks";
-import Shop from "@/assets/icons/shop/shop_icon.svg?react";
-import Tasks from "@/assets/icons/tasks/tasks_icon.svg?react";
-import "./styles.scss";
-import Profile from "../../pages/Profile/Profile";
-import WebApp from "@twa-dev/sdk";
+﻿import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import RouterTab from '../RouterTab/RouterTab';
+import Store from '../../pages/Store/Store';
+import Task from '../../pages/Tasks/Tasks';
+import Shop from '@/assets/icons/shop/shop_icon.svg?react';
+import Tasks from '@/assets/icons/tasks/tasks_icon.svg?react';
+import './styles.scss';
+import Profile from '../../pages/Profile/Profile';
+import WebApp from '@twa-dev/sdk';
 
 function TabBar() {
   const location = useLocation();
-  const selected = location.pathname.split("/")[1];
+  const selected = location.pathname.split('/')[1];
   const params = new URLSearchParams(WebApp.initData);
-  const userData = JSON.parse(params.get("user") || "null");
+  const userData = JSON.parse(params.get('user') || 'null');
 
   return (
     <>
-      <div style={{ width: "100%" }} key={location.pathname}>
+      <div style={{ width: '100%' }} key={location.pathname}>
         <div className="tabbar">
           <RouterTab to="/tasks">
             <div className="tabbar__tab">
               <Tasks
+                style={{ width: '16px', height: '16px' }}
                 className={`icon-${
-                  selected === "tasks" ? "active" : "inactive"
+                  selected === 'tasks' ? 'active' : 'inactive'
                 }`}
               />
               <p>задания</p>
             </div>
           </RouterTab>
           <RouterTab to="/profile">
-              <img src={userData.photo_url} alt="профиль" className="tabbar__avatar"/>
+            <img
+              src={userData.photo_url}
+              alt="профиль"
+              className="tabbar__avatar"
+            />
           </RouterTab>
           <RouterTab to="/store">
             <div className="tabbar__tab">
               <Shop
+                style={{ width: '19px', height: '19px' }}
                 className={`icon-${
-                  selected === "store" ? "active" : "inactive"
+                  selected === 'store' ? 'active' : 'inactive'
                 }`}
               />
               <p>магазин</p>
