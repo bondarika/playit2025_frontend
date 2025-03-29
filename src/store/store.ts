33ef3a692ed950f41470e3f8ab5e0b9aa1984a07
bondarika <bondarika@yandex.ru>
2025-03-29 17:19:19 +0300

    fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -15,28 +15,32 @@ class UserStore {
     });
   }
 
-  async authenticate(userData: { id: number; username: string }) {
+  authenticate = async (userData: { id: number; username: string }) => {
     try {
       const loginResponse = await makeRequest(userData);
       console.log('Login Response:', loginResponse);
+
       const fetchedUser = await fetchUserData();
       if (!fetchedUser) {
         throw new Error('Fetched user data is undefined');
       }
+
       this.user = fetchedUser;
       this.error = null;
+
       document.cookie = `user_id=${fetchedUser.id}; path=/; max-age=259200`;
-    } catch (error) {
+    } catch (error: any) {
+      console.error('Error during authentication:', error);
       this.error =
         error instanceof Error ? error.message : 'Неизвестная ошибка';
     }
-  }
+  };
 
-  clearUser() {
+  clearUser = () => {
     this.user = null;
     this.error = null;
     document.cookie = 'user_id=; path=/; max-age=0';
-  }
+  };
 }
 
 const userStore = new UserStore();
