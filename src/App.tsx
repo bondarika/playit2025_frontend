2b8892bebbe5836bd50943159a26235983af4a81
bondarika <bondarika@yandex.ru>
2025-04-11 12:00:49 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import TabBar from './components/TabBar/TabBar';
 import WebApp from '@twa-dev/sdk';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(() => {
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (!isMobile) {
-      document.body.innerHTML = `
-        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; text-align: center;">
-          <p style="font-family: 'SF Pro Display', Arial, sans-serif; font-size: 20px; font-weight: 600; color: rgba(207, 80, 105, 1);">
-            PlayIT доступно только на мобильных устройствах 😔
-          </p>
-        </div>
-      `;
-    }
-  }, []);
+  // useEffect(() => {
+  //   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  //   if (!isMobile) {
+  //     document.body.innerHTML = `
+  //       <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; text-align: center;">
+  //         <p style="font-family: 'SF Pro Display', Arial, sans-serif; font-size: 20px; font-weight: 600; color: rgba(207, 80, 105, 1);">
+  //           PlayIT доступно только на мобильных устройствах 😔
+  //         </p>
+  //       </div>
+  //     `;
+  //   }
+  // }, []);
   WebApp.expand();
   return (
     <BrowserRouter>
